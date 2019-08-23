09221a908a712036fe8d2ca2f237a3ddb5968974
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-23 12:25:22 -0700

    no sirve este

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ alert("equsidejajasalu2");
 
 let btnC = document.getElementById('btnC');
 
-btnC.addEventListener('click' , ()=>{console.log('dooooooo')} , )
+btnC.addEventListener('click' , ()=>{alert('fredi')});
