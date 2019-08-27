401262388635cc4ca029f74a50d76caf01cf8d54
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-27 12:40:53 -0700

    Elpasodelgigante

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const buttons = document.querySelectorAll('button');
 
 const playNote = event =>{
-    console.log(event.target.dataset.note);
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = (`audio${note}`);
+    console.log(`audio${note}`);
+    const audio = document.getElementById(audioId);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 }
 
 buttons.forEach(
     button  =>  button.addEventListener('click' , playNote)
 );
+
